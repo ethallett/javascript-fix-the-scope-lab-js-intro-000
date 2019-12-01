3c7277b1ca7d4ae43990ecf4518575a94093a73b
Elizabeth T Hallett <ethallett@gmail.com>
2019-12-01 18:11:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,8 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
-  }
+  return function('FUNKY!');
+  
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
